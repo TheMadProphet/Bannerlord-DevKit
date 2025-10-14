16b7734ab59b34d9ce53f831f69bedb5f2a95d39
TheMadProphet <TheMadProphet@users.noreply.github.com>
2025-10-14 23:52:04 +0400

    Add print mods button

--- DevKit/Debuggers/Windows/WindowManager.cs
+++ DevKit/Debuggers/Windows/WindowManager.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using TaleWorlds.Engine;
 using TaleWorlds.Library;
+using TaleWorlds.ModuleManager;
 
 namespace DevKit.Debuggers.Windows;
 
@@ -26,13 +27,36 @@ public class WindowManager : DebuggerWindow
 
         Imgui.Text("Other");
         Imgui.Separator();
-        Button("BREAK", Break, "Break into the debugger to evaluate custom code / game state.");
+        Button(
+            "Debugger.Break()",
+            Break,
+            "Break into the debugger (if attached) to evaluate custom code / game state."
+        );
+        Imgui.SameLine(0, 10);
+        Button("Print Modules", PrintMods, "Prints all loaded submodules.");
     }
 
-    private void Break()
+    private static void Break()
     {
         if (Debugger.IsAttached)
+        {
             Debugger.Break();
+        }
+    }
+
+    private static void PrintMods()
+    {
+        var moduleInfos = ModuleHelper.GetModuleInfos(
+            TaleWorlds.Engine.Utilities.GetModulesNames()
+        );
+
+        InformationManager.DisplayMessage(new InformationMessage("Current Modules:"));
+        foreach (var moduleInfo in moduleInfos)
+        {
+            InformationManager.DisplayMessage(
+                new InformationMessage($"  - {moduleInfo.Name} ({moduleInfo.Version})")
+            );
+        }
     }
 
     [CommandLineFunctionality.CommandLineArgumentFunction("manager", "devkit")]
