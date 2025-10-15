d2f3033b14731eb8f52889a65ec5bb96c59280c4
TheMadProphet <TheMadProphet@users.noreply.github.com>
2025-10-15 13:49:48 +0400

    Enable development mode

---
+++ DevKit/Utilities/PatchDevelopmentMode.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace DevKit.Utilities;
+
+[HarmonyPatch]
+public class PatchDevelopmentMode
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MBGameManager), "get_IsDevelopmentMode")]
+    private static void EnableDevelopmentMode(ref bool __result)
+    {
+        __result = true;
+    }
+}
